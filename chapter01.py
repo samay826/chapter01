bf669965b40a9965f6e76afd914793f8a2cf371e
blackhat hacker <71082274+samay826@users.noreply.github.com>
2021-03-01 01:00:53 +0530

    Add files via upload

---
+++ chapter01.py
@@ -0,0 +1,58 @@
+print("hello world") # first lesson 
+# modules is a file containing code written by somebody 
+# else (usually) which can be imported and used in our programs
+
+# there are two types of modules
+# built in modules # import os
+# external modules
+
+# TOPICS-1  comments 
+#
+
+# comments in one method # 1
+''' your are selected
+in next line line also '''# 2 method
+#
+
+
+# REPL :- read evaluate print loop
+
+
+# moducules
+
+# question1 
+# 1:- print twinkle twinkle little star in python program !
+#ans:- 
+
+# 1 method to print
+print('''the first method of print''')
+
+# ans of first question is downside
+
+print('''Twinkle twinkle little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle little star''')# using 2 method
+
+# 3 rd question {external module}
+
+from playsound import playsound
+playsound('D:\\python\\1. Chapter 1\\play.mp3')
+
+
+# question 4 
+import os
+print(os.listdir())
+
+
+# question 5 
+
+
+# CEH certified ethical hacker
+# vrushabh oswal 
+# harsh varshaan 
+# ubuntu 
+# kali linux
+import os
+print(os.listdir())
---
+++ chapter02.py
@@ -0,0 +1,98 @@
+# chapter 02 variables and data types
+
+harsh = 40 # variable = container to store value
+print(harsh)
+# reserved words jese def 
+
+# datatypes 
+# intergers :- -5 -4 -3 -2 -1 0 1 2 3 4 5 
+# floating numbers in python :- 90.4
+# 56.76 89.74
+# strings :- "harsh" 'harsh' '''harsh'''
+# booleans :- true false , none
+ 
+# examples
+a = 40
+print(type(a)) # for seeing the type of data enter
+# (type(a)) 
+
+harsh = 5000
+print(type(harsh))
+bsdk = 90.4
+print(type(bsdk))
+
+# arithmetic operators 
+# = + , / ,etcself.
+
+#rishabh = 40
+#print("the number of your is :- ",rishabh)
+#arsh = 15
+#print("you r not allowed:- ",harsh)
+
+#a = 10
+#b = 40
+#
+# print(a+b)
+#b = 5
+#d = 4
+#print(b+d)
+a = 101
+a +=16
+print(a)
+
+vrushabh = 40
+vrushabh *= 2
+print(vrushabh)
+
+# new program
+
+ 
+# glitch
+
+samay = 80
+samay += 20
+print(samay)
+
+# 
+khushi = 100
+khushi += 100
+print("mei tumhne bahut pyaar karta hu :- ",(khushi))
+
+
+harsh = 4
+harsh += 100
+print("the value of harsh is :-",harsh)
+
+harsh = 100
+harsh /= 10
+print("the answer is:-",harsh)
+
+print(type(harsh))
+
+h = 9
+h *= 5
+print("the answer is :-",h)
+# above all are the examples 
+
+# assignment operator
+
+a = (30>100)
+print("your answer is ",a) # samaj gaya hoga 
+
+lakshay = (80>20)
+print("wrong answer",lakshay)
+
+
+# typecasting
+
+# 
+# "31" = string literal
+# 31 = numeric literal
+
+# input function 
+
+harsh = input("enter the number :- ")# string mei save hoti 
+harsh = int(harsh)
+y = (harsh+20)
+print("tumhne joh value dali uska 20 double :- ",y)
+
